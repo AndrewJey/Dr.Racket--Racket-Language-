ad4eafcdfc293d1814fa78f7a9e67e060794c285
Andrés Jozef Jiménez Leandro <AndrewJey@gmail.com>
2016-11-16 18:59:32 -0600

    Nuevo Ejemplo de Listas - Andrés Matta
    Nuevo Ejemplo de Listas - Andrés Matta

---
+++ Listas en Dr.Racket (LISP)/EjemploClaseVirtual.rkt
@@ -0,0 +1,16 @@
+#lang racket
+(define l1 (list 1 2 3 4 5 6 7 8 9 10 11))
+(define l2 (list 1 2 3))
+(define l3 (list 4 5 6))
+
+(define (menor-cuatro n) (< n 4 ))
+
+(define (factorial  x)
+  (if (= x 0 ) 1
+      (* x (factorial (- x 1 )))))
+
+(define (sumarlista l)
+(if (empty? l) 0
+    (+ (first l) (sumarlista (rest l)))))
+
+
